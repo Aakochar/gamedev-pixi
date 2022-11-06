a893d8f0afef84e9f844375011e857ff58c04600
Alex Link <rozrobka@yahoo.com>
2022-11-06 23:51:25 +0200

    wip tile map generator

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,56 @@
-import{ Application, Sprite } from 'pixi.js';
-import test from './sprites/test.png';
+import { autoDetectRenderer, Loader, Container, Ticker } from 'pixi.js';
+import type { AbstractRenderer } from 'pixi.js';
+import { CompositeTilemap } from '@pixi/tilemap';
+import wall from './sprites/wall.png';
 
-const app = new Application({ width: 640, height: 360 });
+function makeTilemap(tilemap: CompositeTilemap, resources: Loader['resources']) {
+  tilemap.clear();
+  const size = 32;
 
-const sprite = Sprite.from(test);
-app.stage.addChild(sprite);
+  const pxW = renderer.screen.width;
+  const pxH = renderer.screen.height;
+  const tileW = pxW / size;
+  const tileH = pxH / size;
 
-// Add a ticker callback to move the sprite back and forth
-let elapsed = 0.0;
-app.ticker.add((delta) => {
-  elapsed += delta;
-  sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+  const wallTexture = resources.wall.texture;
+
+  if (!wallTexture) {
+    throw new Error('Failed to load wall texture');
+  }
+
+  for (let i = 0; i < tileW; i++) {
+    for (let j = 0; j < tileH; j++) {
+      if (Math.random() > 0.8) {
+        tilemap.tile(wallTexture, i * size, j * size);
+      }
+    }
+  }
+}
+
+function main(renderer: AbstractRenderer) {
+
+  const loader = new Loader();
+  loader.add('wall', wall);
+
+  loader.load((_, resources) => {
+    const stage = new Container();
+    const tilemap = new CompositeTilemap();
+    stage.addChild(tilemap);
+  
+    Ticker.shared.add(() => renderer.render(stage));
+  
+    makeTilemap(tilemap, resources);
+  });
+}
+
+const renderer = autoDetectRenderer({
+  antialias: true,
+  autoDensity: true,
+  resolution: window.devicePixelRatio || 1,
+  width: 800,
+  height: 600,
 });
 
-document.body.appendChild(app.view as HTMLCanvasElement);
+document.body.appendChild(renderer.view as HTMLCanvasElement);
+
+main(renderer);
